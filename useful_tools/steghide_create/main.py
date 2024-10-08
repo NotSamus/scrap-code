d3f3eb83760522d624b745284d1eb5fe9e419de8
NotSamus <jlopez126@miners.utep.edu>
2024-10-08 17:26:26 -0600

    Added the script for steghide

--- useful_tools/steghide_create/main.py
+++ useful_tools/steghide_create/main.py
@@ -1,22 +1,30 @@
 import os
-# Remember we have to pass a array or a tuple
-def create_process(args):
-        pid = os.fork()
-        if pid > 0:
-            os.waitpid(pid,0)
-        elif pid == 0:
-            os.execv(args[0],args)
 
-def reader(directory):
-    for filename in os.listdir(directory):
-        file_path = os.path.join(directory, filename)
-        if os.path.isfile(file_path):
-            create_process(["/opt/local/bin/steghide", " "])
-
+def reader():
+    print(os.getcwd())
+    i=1
+    for filename in os.listdir('steg_pictures'):
+        os.system(f'steghide embed -cf steg_pictures/n_cat_{i}.jpg -ef flags_separated/secret_{i}.txt -p "" ' )
+        i+=1
+            
+def flag_separator():
+    if os.path.isdir('flags_separated'):
+        os.system('rm -rf flags_separated')
+        os.mkdir('flags_separated')
+    else:
+        os.mkdir('flags_separated')
+    with open("Flags.txt",'r') as file:
+        i=1
+        for line in file:
+            path=f'flags_separated/secret_{i}.txt'
+            with open(path, 'w') as file:
+                file.write(f"{line}")
+                print(f"{path}")
+                i+=1
+            
 def main():
-    directory = "/Users/lopez/projects/scrap-code/useful_tools/steghide_create/resized_pictures"
-    # os.chdir(directory)
-    # create_process(["/bin/ls"])
-    # reader(directory)
+    # directory = "/Users/lopez/projects/scrap-code/useful_tools/steghide_create/"
+    flag_separator() #This is to separate the files from the flag file
+    reader()
 if __name__ == "__main__":
     main()
--- useful_tools/steghide_create/testing/t.py
+++
@@ -1,8 +0,0 @@
-import os
-def main():
-    os.system(' steghide embed -cf n_cat_2.jpg -ef secret.txt -p "" ' )
-    
-if __name__ == "__main__":
-    main()
-    
-    
